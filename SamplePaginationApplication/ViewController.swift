38347e0bfb34d13df34f6ef893b55a59874591b2
Rubha Selvaraj <rubhas@maveric-systems.com>
2024-05-29 17:30:00 +0530

    Initial Commit

---
+++ SamplePaginationApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SamplePaginationApplication
+//
+//  Created by Rubha Selvaraj on 29/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
